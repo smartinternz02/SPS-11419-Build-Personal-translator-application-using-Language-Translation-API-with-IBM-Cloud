125bf36fbe98c9a8bc1fb6fa861abc7386760f1d
ritikasarkar18 <ritikasarkar438@gmail.com>
2021-08-12 20:48:55 +0530

    some more configs

--- languages.py
+++ languages.py
@@ -1,10 +1,11 @@
 # returns a list of languages supported by the translator service
 import requests
 import json
+import config
 
 url = "https://language-translation.p.rapidapi.com/translateLanguage/supported-language"
 headers = { 
-    'x-rapidapi-key': "6baae6a51fmsha48ab291ec53a9fp1b09b7jsn9b56d5c35aa3",
+    'x-rapidapi-key': config.apikey,
     'x-rapidapi-host': "language-translation.p.rapidapi.com" 
     } 
 response = requests.request("GET", url, headers=headers) 
--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 # for testing the detect language service
 import requests 
+import config
 
 url = "https://language-translation.p.rapidapi.com/translateLanguage/detect-language"
 querystring = {"text":"Привет, мой дорогой друг!"} 
 headers = {
-    'x-rapidapi-key': "6baae6a51fmsha48ab291ec53a9fp1b09b7jsn9b56d5c35aa3",
+    'x-rapidapi-key': config.apikey,
     'x-rapidapi-host': "language-translation.p.rapidapi.com"
     } 
 
